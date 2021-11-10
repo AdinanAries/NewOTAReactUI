00edbc92469f76104d07819780fcf2326eedaaa4
Mohammed Adinan <adinanaries@outlook.com>
2021-11-09 22:58:59 -0500

    added site logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,7 @@
 import { show_main_mobile_menu } from "./MobileMenu";
 
+import WillgoLogo from '../WillgoLogo.png';
+
 import { show_login_page, show_home_page, show_trips_page } from '../helpers/PageRoutingFuncs';
 
 function Header(){
@@ -8,7 +10,15 @@ function Header(){
             <div className="wrapper">
                 <div className="header_content_container" style={{display: "flex", justifyContent: "space-between"}}>
                     <div style={{height: "60px", display: "flex", flexDirection: "column", justifyContent: "center"}}>
-                        <p style={{color: ""}}>Willgo.com</p>
+                        <div style={{display: "flex"}}>
+                            <p><img src={WillgoLogo} style={{width: 60}}/></p>
+                            <div style={{marginLeft: 5, display: "flex", flexDirection: "column", justifyContent: "center"}}>
+                                <p style={{color: "rgb(44, 107, 128)", fontFamily: "Sans-serif", fontWeight: "1000", fontSize: 23,}}>
+                                    Willgo
+                                    <span style={{color: "#c900b0"}}>.com</span>
+                                </p>
+                            </div>
+                        </div>
                     </div>
                     
                     <div className="header-menu-items">
