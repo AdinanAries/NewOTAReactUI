7dd62ff9b60ddcede514dfbf1c3485b7b45de556
Mohammed Adinan <adinanaries@outlook.com>
2021-11-15 16:07:31 -0500

    added checkout page

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import LoginPage from './components/LoginPage';
 import ExplorePage from './components/ExplorePage';
 import SearchPage from './components/SearchPage';
 import SelectedTicketPane from './components/SelectedTicketPane';
+import CheckoutPage from './components/CheckoutPage';
 
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
     <div className="">
       <SelectedTicketPane />
       <MobileMenu />
+      <CheckoutPage />
       <Header />
       <HomePage />
       <TripsPage />
